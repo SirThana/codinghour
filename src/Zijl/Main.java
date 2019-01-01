e179279aee6a2d7b03b42c37d7e7c6d286735c67
SirThana <martijn.zijl.3000@gmail.com>
2019-01-01 01:24:53 +0100

    n

--- src/Zijl/Main.java
+++ src/Zijl/Main.java
@@ -9,6 +9,8 @@ public class Main {
         int[] x = new int[] {5,4,3,5,7};
         Arraylist b = new Arraylist("myArray",x);
         Arraylist c = new Arraylist("mySecondArray",x);
+
+        
         System.out.println(b.getNaam());
         System.out.println(c.getNaam());
 
